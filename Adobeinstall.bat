762c57f7a737a53e9e3be0d0c80f6c5e9dc764ce
Ben Snape <51653872+BenSnape@users.noreply.github.com>
2020-05-20 16:16:09 +0100

    Update and rename Adobe install to Adobeinstall.bat

---
+++ Adobeinstall.bat
@@ -0,0 +1,4 @@
+# Get the latest download from Adobe
+# Package as a Win32 App
+# Use the below command for the install command, update the .exe as required
+AcroRdrDC2000620042_en_US.exe /sAll /rs /rps /msi /norestart /quiet EULA_ACCEPT=YES
