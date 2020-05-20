346a58626fd677e4b9bf46a4cc30d9b10cac5466
Ben Snape <51653872+BenSnape@users.noreply.github.com>
2020-05-20 16:18:56 +0100

    Update Adobeinstall.bat

--- Adobeinstall.bat
+++ Adobeinstall.bat
@@ -1,4 +1,4 @@
-# Get the latest download from Adobe
-# Package as a Win32 App
-# Use the below command for the install command, update the .exe as required
+Rem # Get the latest download from Adobe
+Rem # Package as a Win32 App
+Rem # Use the below command for the install command, update the .exe as required
 AcroRdrDC2000620042_en_US.exe /sAll /rs /rps /msi /norestart /quiet EULA_ACCEPT=YES
