28e21ede225d144ebe31f57c2cd66fdc759196ca
Ben Snape <51653872+BenSnape@users.noreply.github.com>
2020-06-16 21:55:22 +0100

    Notes

--- MMDDeviceREadiness.ps1
+++ MMDDeviceREadiness.ps1
@@ -1,4 +1,6 @@
+# Create a folder structure
 New-Item -ItemType "directory" -Path "c:\DeviceReadinessAssessmentTool\$env:computername"
+#Move the Tool files and folders
 Move-Item -Path -Destination "c:\DeviceReadinessAssessmentTool\$env:computername"
 Move-Item -Path Config -Destination "c:\DeviceReadinessAssessmentTool\$env:computername"                                          
 Move-Item -Path Report -Destination "c:\DeviceReadinessAssessmentTool\$env:computername"
@@ -11,3 +13,5 @@ Move-Item -Path Microsoft.MMD.DeviceReadinessAssessmentTool.exe.config -Destinat
 Move-Item -Path Microsoft.MMD.DeviceReadinessAssessmentTool.pdb -Destination "c:\DeviceReadinessAssessmentTool\$env:computername"
 Move-Item -Path Newtonsoft.Json.dll -Destination "c:\DeviceReadinessAssessmentTool\$env:computername"
 Move-Item -Path System.Management.Automation.dll -Destination "c:\DeviceReadinessAssessmentTool\$env:computername"
+# Run the Tool, there is no silent switch
+start-process "c:\DeviceReadinessAssessmentTool\$env:computername\Microsoft.MMD.DeviceReadinessAssessmentTool.exe"
