741b1cfdea9a1b28dae8063e3fbf2b83ad6f126e
Ben Snape <51653872+BenSnape@users.noreply.github.com>
2020-06-30 17:04:29 +0100

    Update Adobeinstall.bat

--- Adobeinstall.bat
+++ Adobeinstall.bat
@@ -5,4 +5,3 @@ AcroRdrDC2000620042_en_US.exe /sAll /rs /rps /msi /norestart /quiet EULA_ACCEPT=
 Rem # get the MSI identifier get-wmiobject Win32_Product | Sort-Object -Property Name | Format-Table IdentifyingNumber, Name, LocalPackage -AutoSize
 Rem # Uninstall command 
 msiexec /x {MSI ID} /q
-msiexec /x {AC76BA86-7AD7-1033-7B44-AC0F074E4100} /q
