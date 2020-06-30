5c994a56c18f98eb1a5a2da3012434dbb57af125
Ben Snape <51653872+BenSnape@users.noreply.github.com>
2020-06-30 17:04:12 +0100

    Update Adobeinstall.bat

--- Adobeinstall.bat
+++ Adobeinstall.bat
@@ -2,3 +2,7 @@ Rem # Get the latest download from Adobe https://get.adobe.com/uk/reader/enterpr
 Rem # Package as a Win32 App
 Rem # Use the below command for the install command, update the .exe as required
 AcroRdrDC2000620042_en_US.exe /sAll /rs /rps /msi /norestart /quiet EULA_ACCEPT=YES
+Rem # get the MSI identifier get-wmiobject Win32_Product | Sort-Object -Property Name | Format-Table IdentifyingNumber, Name, LocalPackage -AutoSize
+Rem # Uninstall command 
+msiexec /x {MSI ID} /q
+msiexec /x {AC76BA86-7AD7-1033-7B44-AC0F074E4100} /q
