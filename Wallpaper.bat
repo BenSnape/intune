a614257fc8141ea428f4d44d05fdf799ec3b26fe
Ben Snape <51653872+BenSnape@users.noreply.github.com>
2020-07-03 15:36:51 +0100

    Update Wallpaper.bat

--- Wallpaper.bat
+++ Wallpaper.bat
@@ -1,3 +1,4 @@
 rem add policy as https://www.petervanderwoude.nl/post/easily-configure-desktop-and-lock-screen-image-via-windows-10-mdm/rem Copy Files
+rem Package with jpg to Win32App
 New-Item -ItemType "directory" -Path "c:\MDMWallpaper"
 copy Desktop.jpg c:\MDMWallpaper /Y
