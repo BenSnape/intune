87258c9e667f1b725a4c5ed204237f261804f003
Ben Snape <51653872+BenSnape@users.noreply.github.com>
2020-09-11 15:12:19 +0100

    Create forticlient.bat

---
+++ forticlient.bat
@@ -0,0 +1,3 @@
+### Install forticlient and apply configuration
+### https://kb.fortinet.com/kb/documentLink.do?externalID=FD41256
+FortiSSLVPNclient.exe connect -s TMCP-VPN -h vpn.domain.com i -m -q
