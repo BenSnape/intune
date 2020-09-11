f53e179f32af8e1f2cec1863641dbaf12b8856f4
Ben Snape <51653872+BenSnape@users.noreply.github.com>
2020-09-11 15:34:13 +0100

    Update forticlient.bat

--- forticlient.bat
+++ forticlient.bat
@@ -1,3 +1,7 @@
 ### Install forticlient and apply configuration
 ### https://kb.fortinet.com/kb/documentLink.do?externalID=FD41256
 FortiSSLVPNclient.exe connect -s TMCP-VPN -h vpn.domain.com i -m -q
+
+Rem # get the MSI identifier get-wmiobject Win32_Product | Sort-Object -Property Name | Format-Table IdentifyingNumber, Name, LocalPackage -AutoSize
+Rem # Uninstall command 
+msiexec /x {MSI ID} /q
