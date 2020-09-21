fd61336eda652942dd221f5d32fbcd44d8def9a0
Ben Snape <51653872+BenSnape@users.noreply.github.com>
2020-09-21 11:14:54 +0100

    Create ExpandArchive.ps1

---
+++ ExpandArchive.ps1
@@ -0,0 +1,3 @@
+## Useful when moving large amounts of data
+## Package as Win32 with zip file, update name and location as needed
+Expand-Archive -LiteralPath files.zip -DestinationPath "C:\ProgramData\"
